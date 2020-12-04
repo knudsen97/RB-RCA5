312b4563f4a8402f138a4d36f229ed4b069f2ccf
Claus Huynh <claus.huynh@yahoo.dk>
2020-12-04 20:27:28 +0100

    removed a chunck of comment

--- src/lidar.cpp
+++ src/lidar.cpp
@@ -27,32 +27,6 @@ void lidar::lidarCallback(ConstLaserScanStampedPtr &msg)
     }
 }
 
-// lidar::lidarData lidar::lidarMin(ConstLaserScanStampedPtr &msg)
-// {
-//     lidar::lidarData data;
-//     float angle_min = float(msg->scan().angle_min());
-//     float angle_increment = float(msg->scan().angle_step());
-
-//     //float range_min = float(msg->scan().range_min());
-//     float range_max = float(msg->scan().range_max());
-//     int nranges = msg->scan().ranges_size();
-//     float minAngle = 0;
-//     float minRange = range_max;
-
-//     for(int i = 0; i < nranges; i++)
-//     {
-//         if(float(msg->scan().ranges(i) < minRange))
-//         {
-//             minRange = float(msg->scan().ranges(i));
-//             minAngle = angle_min + i * angle_increment;
-//         }
-//     }
-//     data.range = minRange; 
-//     data.angle = minAngle;
-
-//     return data;
-// }
-
 
 
 
