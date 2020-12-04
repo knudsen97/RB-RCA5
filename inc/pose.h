0a1a68f5d6f3fdad57da468603a646ceb0fd3fd9
Claus Huynh <claus.huynh@yahoo.dk>
2020-12-04 20:23:36 +0100

    updated pose class to return euler angle

--- inc/pose.h
+++ inc/pose.h
@@ -2,6 +2,9 @@
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/transport/transport.hh>
 #include <array>
+#include <mutex>
+#include <cmath>
+
 
 #ifndef POSE_H
 #define POSE_H
@@ -16,10 +19,16 @@ public:
     void poseCallback(ConstPosesStampedPtr &msg);
     double getPose(int i);
 
-    enum dataType {x, y, z, w, thetaX, thetaY, thetaZ};
+    enum dataType {x, y, z, thetaX, thetaY, thetaZ};
+    /**
+     * the angles range is [-pi, pi]
+     */
 
 private:
+    void angleCal(double w, double x, double y, double z);
     std::array<double, 6> data;
-};
+    //std::mutex poseMutex;
+}
 
 #endif
+
--- src/pose.cpp
+++ src/pose.cpp
@@ -1,4 +1,6 @@
 #include "inc/pose.h"
+#define PI 3.1415926535897932384626
+
 
 pose::pose()
 {
@@ -7,6 +9,7 @@ pose::pose()
 
 void pose::poseCallback(ConstPosesStampedPtr &_msg)
 {
+//    poseMutex.lock();
     for (int i = 0; i < _msg->pose_size(); i++) 
     {
         if (_msg->pose(i).name() == "pioneer2dx") 
@@ -14,16 +17,42 @@ void pose::poseCallback(ConstPosesStampedPtr &_msg)
             data[pose::x] = _msg->pose(i).position().x();
             data[pose::y] = _msg->pose(i).position().y();
             data[pose::z] = _msg->pose(i).position().z();
-            data[pose::w] = _msg->pose(i).orientation().w();
-            data[pose::thetaX] = _msg->pose(i).orientation().x();
-            data[pose::thetaY] = _msg->pose(i).orientation().y();
-            data[pose::thetaZ] = _msg->pose(i).orientation().z();
+            //convert to auler angles
+            angleCal(
+                _msg->pose(i).orientation().w(), 
+                _msg->pose(i).orientation().x(), 
+                _msg->pose(i).orientation().y(), 
+                _msg->pose(i).orientation().z()
+            );
         }
     }
+//    poseMutex.unlock();
 }
 
 double pose::getPose(int i)
 {
+//    poseMutex.lock();
     return data[i];
+//    poseMutex.unlock();
 }
+
+void pose::angleCal(double w, double x, double y, double z){
+
+// calculate theta_X
+double sinr_cosp = 2 * (w * x + y * z);
+double cosr_cosp = 1 - 2 * (x * x + y * y);
+this->data[pose::thetaX] = std::atan2(sinr_cosp, cosr_cosp);
+
+// calculate theta_Y
+double sinp = 2 * (w * y - z * x);
+if (std::abs(sinp) >= 1)
+    this->data[pose::thetaY] = std::copysign(PI / 2, sinp); // use PI rad if out of range
+else
+    this->data[pose::thetaY] = std::asin(sinp);
+
+// calculate theta_Z
+double siny_cosp = 2 * (w * z + x * y);
+double cosy_cosp = 1 - 2 * (y * y + z * z);
+this->data[pose::thetaZ] = std::atan2(siny_cosp, cosy_cosp);
+};
 
